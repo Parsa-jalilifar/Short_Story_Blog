c3f9c41760771747b4003b9066159b24dbb05b90
Parsa-jalilifar <parsajalilifar@gmail.com>
2021-07-29 23:05:18 -0400

    basic register and login handling added

--- shortstoryblog/users/routes.py
+++ shortstoryblog/users/routes.py
@@ -1,13 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for, flash, redirect
+from shortstoryblog.users.forms import RegistrationForm, LoginForm
 
 users = Blueprint("users", __name__)
 
 
-@users.route("/login")
+@users.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html", title="Login")
+    form = LoginForm()
+    return render_template("login.html", title="Login", form=form)
 
 
-@users.route("/register")
+@users.route("/register", methods=["GET", "POST"])
 def register():
-    return render_template("register.html", title="Register")
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account Created for {form.username.data}!", "success")
+        return redirect(url_for("main.home"))
+    return render_template("register.html", title="Register", form=form)
