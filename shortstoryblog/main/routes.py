e8ab981e2625f394fd10bed8acaa3171ed71303d
Parsa-jalilifar <parsajalilifar@gmail.com>
2021-07-31 19:02:10 -0400

    basic pagination added

--- shortstoryblog/main/routes.py
+++ shortstoryblog/main/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, url_for
+from flask import Blueprint, render_template, url_for, request
 from flask_login import login_required, current_user
 from shortstoryblog.models import Post
 
@@ -40,5 +40,6 @@ main = Blueprint("main", __name__)
 @main.route("/home")
 @login_required
 def home():
-    posts = Post.query.order_by(Post.date_posted.desc())
+    page = request.args.get("page", 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template("home.html", title="Home", posts=posts)
