8619ecda51e70cc3d3a9d464ef023c1e31e01bc7
Parsa-jalilifar <parsajalilifar@gmail.com>
2021-07-30 23:08:07 -0400

    user and post tables made

--- shortstoryblog/__init__.py
+++ shortstoryblog/__init__.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from shortstoryblog.config import Config
+
+
+db = SQLAlchemy()
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(Config)
+
+
+    db.init_app(app)
 
     from shortstoryblog.main.routes import main
     from shortstoryblog.users.routes import users
