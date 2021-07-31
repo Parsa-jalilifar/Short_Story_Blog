cb17b3b72c5787e7a3b86a939092bcd865cb8a68
Parsa-jalilifar <parsajalilifar@gmail.com>
2021-07-31 15:54:08 -0400

    account page completed

--- shortstoryblog/users/routes.py
+++ shortstoryblog/users/routes.py
@@ -1,8 +1,9 @@
-from flask import Blueprint, render_template, url_for, flash, redirect,request
+from flask import Blueprint, render_template, url_for, flash, redirect, request
 from flask_login import login_user, current_user, logout_user, login_required
-from shortstoryblog.users.forms import RegistrationForm, LoginForm
+from shortstoryblog.users.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from shortstoryblog import db, bcrypt
 from shortstoryblog.models import User, Post
+from shortstoryblog.users.utils import save_picture
 
 users = Blueprint("users", __name__)
 
@@ -44,4 +45,26 @@ def register():
 @users.route("/logout")
 def logout():
     logout_user()
-    return redirect(url_for('users.login'))
+    return redirect(url_for("users.login"))
+
+
+@users.route("/account", methods=["GET", "POST"])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash("Your account has been updated!", "success")
+        return redirect(url_for("users.account"))
+    elif request.method == "GET":
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for("static", filename="profile_pics/" + current_user.image_file)
+    return render_template(
+        "account.html", title="Account", image_file=image_file, form=form
+    )
