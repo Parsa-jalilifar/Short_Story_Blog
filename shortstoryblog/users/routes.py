cbb978b21b9433df6f180700175b5eb066aca948
Parsa-jalilifar <parsajalilifar@gmail.com>
2021-07-30 23:26:37 -0400

    user register to database added

--- shortstoryblog/users/routes.py
+++ shortstoryblog/users/routes.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, render_template, url_for, flash, redirect
 from shortstoryblog.users.forms import RegistrationForm, LoginForm
+from shortstoryblog import db, bcrypt
+from shortstoryblog.models import User, Post
 
 users = Blueprint("users", __name__)
 
@@ -14,6 +16,14 @@ def login():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f"Account Created for {form.username.data}!", "success")
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode(
+            "utf-8"
+        )
+        user = User(
+            username=form.username.data, email=form.email.data, password=hashed_password
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash(f"Your Acccount is created!", "success")
         return redirect(url_for("main.home"))
     return render_template("register.html", title="Register", form=form)
