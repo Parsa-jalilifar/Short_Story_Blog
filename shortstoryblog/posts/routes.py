ea0bbf228937fda39c9214e0adeecf0671d93708
Parsa-jalilifar <parsajalilifar@gmail.com>
2021-08-01 16:06:41 -0400

    post completed

--- shortstoryblog/posts/routes.py
+++ shortstoryblog/posts/routes.py
@@ -24,6 +24,12 @@ def new_post():
     )
 
 
+@posts.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template("post.html", title=post.title, post=post)
+
+
 @posts.route("/post/<int:post_id>/update", methods=["GET", "POST"])
 @login_required
 def update_post(post_id):
